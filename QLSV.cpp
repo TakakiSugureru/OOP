d3ef2becf51182521253e7b41594404ff5cfcf87
Arko <60898245+TakakiSugureru@users.noreply.github.com>
2023-07-23 14:49:03 +0700

    QLSV.cpp

--- QLSV.cpp
+++ QLSV.cpp
@@ -1,326 +1,157 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <fstream>
 
 using namespace std;
 
-class SinhVien
-{
+class SV{
+private:
+	int MSSV;
+	string Ten;
+	float DTB, LG, OOP, KTMT;
 public:
-	int MaSo;
-	string HoTen;
-	float TKLG, LTHDT, KTMT;
-	float DTB;
-
-public:
-	SinhVien(int MaSo,string HoTen,float TKLG,float LTHDT, float KTMT)
-	{
-		this->MaSo= MaSo;
-		this->HoTen=HoTen;
-		this->TKLG=TKLG;
-		this->LTHDT=LTHDT;
-		this->KTMT=KTMT;
-		this->DTB = TinhTrungBinh();
+	SV(int MSSV = 0, string Ten = "", float DTB = -1, float LG = -1, float OOP = -1, float KTMT = -1):
+		MSSV(MSSV), Ten(Ten), DTB(DTB), LG(LG), OOP(OOP), KTMT(KTMT) {
+		}
+	virtual void Nhap(){
+		cout << "Nhap MSSV: "; cin >> MSSV;
+		cout << "Nhap Ho va ten: "; cin >> ws; getline(cin, Ten);
+		cout << "Nhap diem Logic: "; cin >> LG;
+		cout << "Nhap diem OOP: "; cin >> OOP;
+		cout << "Nhap diem KTMT: "; cin >> KTMT;
+		DTB = DiemTB();
 	}
-
-	SinhVien()
-	{
-		this->MaSo=0;
-		this->HoTen="";
-		this->TKLG=-1;
-		this->LTHDT=-1;
-		this->KTMT=-1;
-		this->DTB = TinhTrungBinh();
+	virtual void Xuat(){
+		cout << "MSSV: " << MSSV << endl;
+		cout << "Ho va ten: " << Ten << endl;
+		cout << "Logic: " << LG << endl;
+		cout << "OOP: " << OOP << endl;
+		cout << "KTMT: " << KTMT << endl;
+		cout << "Diem trung binh: " << DTB << endl;
 	}
-//*******************************************************************************************************************************************************************
-
-	virtual void nhap() 
-	{
-		cout << "Ma so sinh vien:";
-		cin >> MaSo;
-		cout << "Ho va Ten:";
-		cin.ignore();
-		getline(cin, HoTen);
-		cout << "Thiet ke logic:";
-		cin >> TKLG;
-		cout << "Lap trinh huong doi tuong:";
-		cin >> LTHDT;
-		cout << "Kien truc may tinh:";
-		cin >> KTMT;
-		DTB = TinhTrungBinh();
+	float DiemTB (){
+		return (LG+OOP+KTMT)/3;
 	}
-
-	virtual void xuat() 
-	{
-		cout << "\n\nMa so sinh vien:" << MaSo;
-		cout << "\nHo va Ten:" << HoTen;
-		cout << "\nThiet ke logic:" << TKLG;
-		cout << "\nLap trinh huong doi tuong:" << LTHDT;
-		cout << "\nKien truc may tinh:" << KTMT;
-		cout << "\nDiem trung binh:" << DTB;
+//===================================================================================
+	int getOOP (){
+		return this->OOP;
 	}
-
-	float TinhTrungBinh()
-	{
-		return (LTHDT+TKLG+KTMT)/3;
+	int getLG (){
+		return this->LG;
 	}
-
-//*******************************************************************************************************************************************************************
-
-	virtual void xuatfile(ofstream &file)
-	{
-		file << MaSo << "\n";
-		file << HoTen << "\n";
-		file << TKLG << "\n";
-		file << LTHDT << "\n";
-		file << KTMT << "\n";
-		file << DTB << "\n";
+	int getKTMT (){
+		return this->KTMT;
 	}
-
-	virtual void docfile(ifstream &file)
-	{
-		file >> MaSo;
-		file.ignore();
-		getline(file,HoTen);
-		file >> TKLG ;
-		file >> LTHDT ;
-		file >> KTMT;
-		file >> DTB;
+	void setOOP (float OOP){
+		this->OOP = OOP;
 	}
-
-		
-	friend istream& operator >> (istream& is, SinhVien &sv)
-	{
-		sv.nhap();
+	void setLG (float LG){
+		this->LG = LG;
+	}
+	void setKTMT (float KTMT){
+		this->KTMT = KTMT;
+	}
+	
+//===================================================================================
+//Nap chong toan tu
+	friend istream& operator >> (istream &is, SV &sv){
+		sv.Nhap();
 		return is;
 	}
-
-	friend ostream& operator << (ostream& os, SinhVien &sv)
-	{
-		sv.xuat();
+	friend ostream& operator << (ostream &os, SV &sv){
+		sv.Xuat();
 		return os;
 	}
 };
 
-//*******************************************************************************************************************************************************************
-
-class SinhVienDaiTra : public SinhVien
-{
+//===================================================================================
+class SVDT: public SV{
+private:
+	float DTB, PPT, BUS, VLSI;
 public:
-	float PPTinh, Bus, VLSI;
-	float DTB;
-
-public:
-	void nhap() 
-	{
-		SinhVien::nhap();
-		cout << "Phuong phap tinh:";
-		cin >> PPTinh;
-		cout << "Kien truc bus:";
-		cin >> Bus;
-		cout << "Thiet ke VLSI:";
-		cin >> VLSI;
-		DTB = TinhTrungBinh();
+	void Nhap(){
+		SV::Nhap();
+		cout << "Nhap diem PPT: "; cin >> PPT;
+		cout << "Nhap diem BUS: "; cin >> BUS;
+		cout << "Nhap diem VLSI: "; cin >> VLSI;
+		DTB = DiemTB();
 	}
-
-	void xuat() 
-	{
-		cout << MaSo <<"\t" <<HoTen<<"\t"<<TKLG<<"\t"<<LTHDT<<"\t"<<KTMT<<"\t"<<PPTinh<<"\t"<<Bus<<"\t"<<VLSI<<"\t"<<DTB;
+	void Xuat(){
+		SV::Xuat();
+		cout << "Diem PPT: " << PPT << endl;
+		cout << "Diem BUS: " << BUS << endl;
+		cout << "Diem VLSI: " << VLSI << endl;
 	}
-
-	float TinhTrungBinh()
-	{
-		return (LTHDT+TKLG+KTMT+PPTinh+Bus+VLSI)/6;
-	}
-
-	void xuatfile(ofstream &file)
-	{
-		file << MaSo << "\n";
-		file << HoTen << "\n";
-		file << TKLG << "\n";
-		file << LTHDT << "\n";
-		file << KTMT << "\n";
-		file << PPTinh << "\n";
-		file << Bus << "\n";
-		file << VLSI << "\n";
-		file << DTB << "\n";
-	}
-
-	void docfile(ifstream &file)
-	{
-		file >> MaSo;
-		file.ignore();
-		getline(file,HoTen);
-		file >> TKLG ;
-		file >> LTHDT ;
-		file >> KTMT;
-		file >> PPTinh;
-		file >> Bus;
-		file >> VLSI;
-		file >> DTB;
-	}
-
-};
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-class SinhVienCLC : public SinhVien
-{
-public:
-	float DSP, Nano;
-	float DTB;
-
-public:
-	void nhap() 
-	{
-		SinhVien::nhap();
-		cout << "DSP:";
-		cin >> DSP;
-		cout << "Cong nghe Nano:";
-		cin >> Nano;
-		DTB = TinhTrungBinh();
-	}
-
-	void xuat() 
-	{
-		cout << MaSo <<"\t" <<HoTen<<"\t"<<TKLG<<"\t"<<LTHDT<<"\t"<<KTMT<<"\t"<<DSP<<"\t"<<Nano<<"\t"<<DTB;
-	}
-
-	float TinhTrungBinh()
-	{
-		return (LTHDT+TKLG+KTMT+(DSP*2)+(Nano*2))/7;
-	}
-
-	void xuatfile(ofstream &file)
-	{
-		file << MaSo << "\n";
-		file << HoTen << "\n";
-		file << TKLG << "\n";
-		file << LTHDT << "\n";
-		file << KTMT << "\n";
-		file << DSP << "\n";
-		file << Nano << "\n";
-		file << DTB << "\n";
-	}
-
-	void docfile(ifstream &file)
-	{
-		file >> MaSo;
-		file.ignore();
-		getline(file,HoTen);
-		file >> TKLG ;
-		file >> LTHDT ;
-		file >> KTMT;
-		file >> DSP;
-		file >> Nano;
-		file >> DTB;
+	float DiemTB(){
+		return (getOOP()+getLG()+getKTMT()+PPT+BUS+VLSI)/6;
 	}
 };
 
-//*******************************************************************************************************************************************************************
-class MENU
-{
+class SVCLC: public SV{
+private:
+	float DTB, DSP, NANO;
 public:
-	vector <SinhVien *> sv;
-	int SiSo, SiSoCLC, SiSoDaiTra;
-
-public:
-	void nhapDS()
-	{
-		cout << "Nhap so sv CLC:";
-		cin >> SiSoCLC;
-		cout << "Nhap so sv dai tra:";
-		cin >> SiSoDaiTra;
-		SiSo = SiSoCLC + SiSoDaiTra;
-		sv.resize(SiSo);
-		
-		for (int i=0; i<SiSoCLC; i++)  sv[i] = new SinhVienCLC;
-		for (int i=SiSoCLC; i<SiSo; i++)  sv[i] = new SinhVienDaiTra;
-		
-		for (int i=0; i<SiSo; i++)  sv[i]->nhap();
+	void Nhap(){
+		SV::Nhap();
+		cout << "Nhap diem DSP: "; cin >> DSP;
+		cout << "Nhap diem NANO: "; cin >> NANO;
+		DTB = DiemTB();
 	}
+	void Xuat(){
+		SV::Xuat();
+		cout << "Diem DSP: " << DSP << endl;
+		cout << "Diem NANO: " << NANO << endl;
+	}
+	float DiemTB(){
+		return (getOOP()+getLG()+getKTMT()+(DSP*2)+(NANO*2))/7;
+	}
+};
 
-	void xuatDS()
-	{
-		for (int i=0; i<sv.size(); i++)
-		{
-			sv[i]->xuat();
+//===================================================================================
+class MENU{
+private:
+	vector <SV *> sv;
+	int SS, SSDT, SSCLC;
+public:
+	void NhapDS(){
+		cout << "Nhap si so SV dai tra: "; cin >> SSDT;
+		cout << "Nhap si so SV clc: "; cin >> SSCLC;
+		SS = SSDT + SSCLC;
+		sv.resize(SS);
+		
+		for(int i = 0; i < SSCLC; i++) sv[i] = new SVDT;
+		for(int i = SSCLC; i < SS; i++) sv[i] = new SVCLC;
+		
+		for(int i = 0; i < SS; i++) sv[i]->Nhap();
+	}
+	void XuatDS(){
+		for(int i = 0; i < sv.size(); i++){
+			sv[i]->Xuat();
 			cout << "\n";
 		}
 		cout << "\n\n";
 		cout << "\n\n\n";
 		system("pause");
-	}
-
-	void inMenu()
-	{
-		char option;
-		do
-		{
+	}	 
+	void inMENU(){
+		char op;
+		do{
 			system("cls");
 			cout << "\n===== MENU =====";
 			cout << "\n1. Nhap danh sach";
 			cout << "\n2. Xuat danh sach";
-			cout << "\n3. Ghi danh sach ra file";
-			cout << "\n4. Doc danh sach tu file";
-			cout << "\n5. Thoat";
+			cout << "\n3. Thoat";
 			cout << "\nNhap lua chon:";
-			cin >> option;
-			switch (option)
-			{
-				case '1':  nhapDS();   break;
-				case '2':  xuatDS();   break;
-				case '3':  GhiDS();   break;
-				case '4':  DocDS();   break;		
+			cin >> op;
+			switch (op){
+				case '1': NhapDS(); break;
+				case '2': XuatDS(); break;
 			}
-		} while(option!='5');
-	}
-
-	void GhiDS()
-	{
-		ofstream file;
-		file.open("d:\\danhsach.txt");
-		if (file.is_open())
-		{
-			file << sv.size() << "\n";
-
-			for(int i=0; i<sv.size(); i++)
-			{ 
-				sv[i]->xuatfile(file);
-			}
-			cout<<"Da ghi xong!!!\n";
-			file.close();
-		}
-		else cout << "Khong mo duoc file!!!\n";
-		system("pause");
-	}
-
-	void DocDS()
-	{
-		ifstream file; int n_sv;
-		file.open("d:\\danhsach.txt");
-		if (file.is_open())
-		{
-			file >> n_sv;
-			sv.resize(n_sv);
-			for(int i=0; i<n_sv; i++)
-			{ 
-				sv[i]->docfile(file);
-			}
-			cout<<"Da doc xong!!!\n";
-			file.close();
-		}
-		else cout << "Khong mo duoc file!!!\n";
-		system("pause");
+		} while(op != '3');
 	}
 };
 
-//*******************************************************************************************************************************************************************
-
-int main()
-{
-	MENU menu1;
-	menu1.inMenu();
+int main(){
+	MENU a;
+	a.inMENU();
 	return 0;
 }
